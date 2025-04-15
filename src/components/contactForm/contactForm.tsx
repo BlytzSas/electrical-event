b58e83d7fdbd2bbe99d02f1a5901cc0d0d0590b0
Cristhian A Luna <dev.cluna@gmail.com>
2025-04-15 17:51:00 -0500

    fix: remove secrets from previous commits (#6)

--- src/components/contactForm/contactForm.tsx
+++ src/components/contactForm/contactForm.tsx
@@ -1,6 +1,9 @@
+import { Fragment } from "react";
 import { useForm } from "@tanstack/react-form";
 import { InputField } from "./inputField";
 import { TextArea } from "./textArea";
+import { Bounce, ToastContainer, toast } from 'react-toastify';
+
 
 type FormField = {
   type?: string;
@@ -18,20 +21,77 @@ export function ContactForm({ formFields }: { formFields: FormField[] }) {
       message: "",
     },
     onSubmit: async ({ value }) => {
-      return value
+      try {
+        const response = await fetch("/api/createContact", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(value),
+        });
+        const data = await response.json();
+
+        if (data.success) {
+          toast.success('Solicitud Exitosa 🚀!', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+            });
+          form.reset();
+        } else {
+          console.log("Error:", data.error.Body.message);
+          toast.error(`Peticion Rechazada ❌: ${data.error.Body.message}`, 
+          {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+          }
+          );
+        }
+      } catch (err) {
+        
+        toast.error('Ocurrió un error al enviar el formulario ❌', {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+          });
+        console.error("Error en la petición:", err);
+      }
     },
   });
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    event.stopPropagation();
+    form.handleSubmit();
+  };
+
+  
+
   return (
-    <form
+    <Fragment>
+      <ToastContainer position="top-center" autoClose={3000} />
+      <form
       className="p-6 flex flex-col items-center justify-between border rounded-md border-(--color-primary) gap-8 w-full md:w-md"
-      action="https://formspree.io/f/xeoapnkw"
-      method="POST"
-      onSubmit={(event) => {
-        event.preventDefault();
-        event.stopPropagation();
-        form.handleSubmit();
-      }}
+      onSubmit={handleSubmit}
     >
       <form.Field name="name">
         {(field) => (
@@ -39,52 +99,47 @@ export function ContactForm({ formFields }: { formFields: FormField[] }) {
             name={field.name}
             type="text"
             value={field.state.value}
-            placeholder={formFields[0].placeholder}
+            placeholder={formFields[0]?.placeholder || "Name"}
             field={field}
-            onChange={(event) =>
-              field.handleChange(event.target.value)
-            }
+            onChange={(event) => field.handleChange(event.target.value)}
           />
         )}
       </form.Field>
+
       <form.Field name="email">
         {(field) => (
           <InputField
             name={field.name}
             type="email"
             value={field.state.value}
-            placeholder={formFields[1].placeholder}
+            placeholder={formFields[1]?.placeholder || "Email"}
             field={field}
-            onChange={(event) =>
-              field.handleChange(event.target.value)
-            }
+            onChange={(event) => field.handleChange(event.target.value)}
           />
         )}
       </form.Field>
+
       <form.Field name="phone">
         {(field) => (
           <InputField
             name={field.name}
             type="text"
             value={field.state.value}
-            placeholder={"Phone"}
+            placeholder={formFields[2]?.placeholder || "Phone"}
             field={field}
-            onChange={(event) =>
-              field.handleChange(event.target.value)
-            }
+            onChange={(event) => field.handleChange(event.target.value)}
           />
         )}
       </form.Field>
+
       <form.Field name="message">
         {(field) => (
           <TextArea
             name={field.name}
             value={field.state.value}
-            placeholder={"Message"}
+            placeholder={formFields[3]?.placeholder || "Message"}
             field={field}
-            onChange={(event) =>
-              field.handleChange(event.target.value)
-            }
+            onChange={(event) => field.handleChange(event.target.value)}
           />
         )}
       </form.Field>
@@ -93,15 +148,17 @@ export function ContactForm({ formFields }: { formFields: FormField[] }) {
         selector={(state) => [state.canSubmit, state.isSubmitting]}
       >
         {([canSubmit, isSubmitting]) => (
-          <button
+      <button
             className="bg-(--color-primary) rounded-md text-(--color-background) px-4 py-2 w-full cursor-pointer"
-            type="submit"
-            disabled={!canSubmit}
+        type="submit"
+      disabled={!canSubmit}
           >
             {isSubmitting ? "..." : "Submit"}
-          </button>
+      </button>
         )}
       </form.Subscribe>
     </form>
+    </Fragment>
+    
   );
 }
